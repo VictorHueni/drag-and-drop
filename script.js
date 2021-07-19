ab25d9686e5a1e8011213ca46c2876432c8bf5ea
Victor Hüni <victor.hueni@protonmail.com>
2021-07-19 20:42:13 +0200

    drag and drop API

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const saveItemBtns = document.querySelectorAll('.solid');
 const addItemContainers = document.querySelectorAll('.add-container');
 const addItems = document.querySelectorAll('.add-item');
 // Item Lists
-const itemLists = document.querySelectorAll('.drag-item-list');
+const listColumns = document.querySelectorAll('.drag-item-list');
 const backlogList = document.getElementById('backlog-list');
 const progressList = document.getElementById('progress-list');
 const completeList = document.getElementById('complete-list');
@@ -21,6 +21,8 @@ let onHoldListArray = [];
 let listArrays = {};
 
 // Drag Functionality
+let draggedItem;
+let currentColumn;
 
 
 // Get Arrays from localStorage if available, set default values if not
@@ -61,9 +63,10 @@ function createItemEl(columnEl, column, item, index) {
     const listEl = document.createElement('li');
     listEl.classList.add('drag-item');
     listEl.textContent = item;
+    listEl.draggable = true;
+    listEl.setAttribute('ondragstart', 'drag(event)');
     //Append
     columnEl.appendChild(listEl);
-
 }
 
 // Update Columns in DOM - Reset HTML, Filter Array, Update localStorage
@@ -99,9 +102,33 @@ function updateDOM() {
     });
 
     // Run getSavedColumns only once, Update Local Storage
-
-
 }
+
+// When item starts draggin
+function drag(e) {
+    draggedItem = e.target;
+}
+// Column allows for Item to drop
+function allowDrop(e) {
+    e.preventDefault();
+}
+// Wheren item enters column area
+function dragEnter(column) {
+    listColumns[column].classList.add('over');
+    currentColumn = column;
+}
+// Dropping item in column
+function drop(e) {
+    e.preventDefault();
+    // remove background Color/Padding
+    listColumns.forEach((column) => {
+        column.classList.remove('over');
+    });
+    // Add item to  Column
+    const parent = listColumns[currentColumn];
+    parent.appendChild(draggedItem);
+}
+
 
 
 // On Load
